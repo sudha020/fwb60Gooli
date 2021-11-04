79556e4ccc3f169a6ecfb7798335a48ad1bd065e
sudha020 <89408777+sudha020@users.noreply.github.com>
2021-11-03 21:10:55 -0500

    added code

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/umbrella', umbrellaRouter);
 app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
