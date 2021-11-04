70509de7df1d7135c63d9dd067a59595e166e9e0
sudha020 <89408777+sudha020@users.noreply.github.com>
2021-11-03 19:53:16 -0500

    addmods

--- routes/addmods.js
+++ routes/addmods.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('addmods', { title: 'Express' });
+  let query = req.query 
+  console.log(`rows ${query.rows}`) 
+  console.log(`cols ${query.cols}`)
+  res.render('addmods', { title: 'Addmods', query: query});
 });
 
 module.exports = router;
